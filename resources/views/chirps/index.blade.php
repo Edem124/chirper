fbaf91157533ab6e0d7e293ccebc166e575fc641
EdemDev <ekpomachi@gmail.com>
2023-08-30 09:48:38 +0100

    commit

--- resources/views/chirps/index.blade.php
+++ resources/views/chirps/index.blade.php
@@ -4,8 +4,8 @@
             @csrf
             <textarea
                 name="message"
-                placeholder="{{ __('What\'s on your mind?') }}"
-                class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                placeholder="{{ __('Your task name') }}"
+                class="block w-full border-gray-200 focus:border-indigo-200 focus:ring focus:ring-indigo-100 focus:ring-opacity-50 rounded-md shadow-sm"
             >{{ old('message') }}</textarea>
             <x-input-error :messages="$errors->get('message')" class="mt-2" />
             <x-primary-button class="mt-4">{{ __('Chirp') }}</x-primary-button>
--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/dashboard', function () {
 Route::resource('chirps', ChirpController::class)
     ->only(['index', 'store', 'edit', 'update', 'destroy'])
     ->middleware(['auth', 'verified']);
-
+    
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
